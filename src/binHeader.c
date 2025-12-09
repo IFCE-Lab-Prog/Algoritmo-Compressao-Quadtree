8d14b27bc33b808e9c6d66d5d9e40932a8740aad
Daniel Freitas <101065507+Dandiels@users.noreply.github.com>
2025-12-09 20:08:06 -0300

    Refactor header assignment to remove type casting

--- src/binHeader.c
+++ src/binHeader.c
@@ -12,8 +12,8 @@ void writeBINHeader(struct pgm *image, struct binHeader *header, FILE *arquivo)
 {
     header->assinatura[0] = 'B';
     header->assinatura[1] = 'S';
-    header->c = (unsigned short) image->c;
-    header->r = (unsigned short) image->r;
+    header->c = image->c;
+    header->r = image->r;
     header->mv = (unsigned char) image->mv;
 
     fprintf(arquivo, "%c%c\n%d %d\n%d\n", header->assinatura[0], header->assinatura[1], header->c, header->r, header->mv);
